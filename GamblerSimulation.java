08f1c098076b23173eda7cc539eb55a82fa88616
Rohan2596 <rohankadam662@gmail.com>
2020-09-26 18:49:33 +0530

    ADDED:Comments to the simulation.

--- GamblerSimulation.java
+++ GamblerSimulation.java
@@ -1,5 +1,9 @@
 public class GamblerSimulation {
 
+    /*
+    * @useCase 1: Setting STAKE as 100 and and BET 1
+    * @author   :Rohan kadam
+    * */
     public static final int STAKE=100;
     public static final  int BET=1;
 
