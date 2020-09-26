7dd16876a6bb74857cff233d8eb9ede3ac580a52
Rohan2596 <rohankadam662@gmail.com>
2020-09-26 18:58:59 +0530

    ADDED:Win or loss method to simualtion.

--- GamblerSimulation.java
+++ GamblerSimulation.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class GamblerSimulation {
 
     /*
@@ -6,5 +8,30 @@ public class GamblerSimulation {
     * */
     public static final int STAKE=100;
     public static final  int BET=1;
+    public static final int WIN=BET;
+    public static final int LOSS=0;
+
+    /*
+    * @useCase 2: Win Or Loss
+    * @author   :Rohan Kadam
+    * */
+
+    public int winOrLoss() {
+        Random random = new Random();
+        int value = random.nextInt(2) + LOSS;
+        if (value == WIN) {
+            System.out.println("***Gambler Won***");
+            return WIN;
+        }
+        System.out.println("***Gambler Loss***");
+        return LOSS;
+
+    }
+
+    public static void main(String[] args) {
+      GamblerSimulation gamblerSimulation=new GamblerSimulation();
+      gamblerSimulation.winOrLoss();
+    }
+
 
 }
